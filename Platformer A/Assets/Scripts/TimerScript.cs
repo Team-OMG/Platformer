4ae41d033e08bfdec27bee73601cf9276a6cc8b0
Daniel P (danielunitydev) <danielpen814@outlook.com>
2019-10-23 17:12:42 +0100

    Timer Script And Display Created

---
+++ Platformer A/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+
+    int Time = 0;
+
+    Text txt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = GetComponent<Text>();
+
+        InvokeRepeating("GameTimer", 0f, 1f);
+        //Calling This Every Second
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void GameTimer()
+    {
+
+        Time++;
+        Debug.Log("Game Has Been Running For: " + Time + " Seconds");
+        txt.text = "Time: " + Time;
+
+    }
+}
