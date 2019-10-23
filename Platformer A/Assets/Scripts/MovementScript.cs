c99ed9376a0cc46ce240c07ce1738e4c87975a01
Daniel P (danielunitydev) <danielpen814@outlook.com>
2019-10-23 15:32:00 +0100

    Level01 Changes
    I Have Created Really Bad Temp Graphics For The Player And Ground Just To Be Used Until The Art Team Get The Proper Art

--- Platformer A/Assets/Scripts/MovementScript.cs
+++ Platformer A/Assets/Scripts/MovementScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void Fixedpdate()
     {
-        
+
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+
+
+
     }
 }
