49a101fb54c0a682dea9b39e03276df74a31cc2e
danielunitydev <danielpen814@outlook.com>
2019-11-10 17:59:12 +0000

    Coin Prefab Changes, Level02 Changes And Script Changes
    Script Created To Give Coins A Bobbing Effect Instead Of Just Floating In The Air, The Coins Now Bob Up And Down...

---
+++ Platformer-A/Assets/Scripts/CoinMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinMovement : MonoBehaviour
+{
+    public float min;
+    public float max;
+    // Use this for initialization
+    void Start()
+    {
+
+        min = transform.position.y;
+        max = transform.position.y + 0.5f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * 1, max - min) + min, transform.position.z);
+
+    }
+    
+}
